df8a1c7bacdb96d55e8ffddccf916f0f2aabf549
Maczi01 <mateusz.w.twardy@gmail.com>
2020-01-12 18:26:47 +0100

    feat: add delegation - now i can chooose city from button

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-// "use strict";
+"use strict";
 
 // service worker registration - remove if you're not going to use it
 
@@ -6,10 +6,10 @@ if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
         navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
             // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            // console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }, function (err) {
             // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
+            // console.log('ServiceWorker registration failed: ', err);
         });
     });
 }
@@ -20,27 +20,32 @@ const temp = document.querySelector('.info__item--temperature--js');
 const humidity = document.querySelector('.info__item--humidity--js');
 const clouds = document.querySelector('.info__item--clouds--js');
 const wind = document.querySelector('.info__item--wind--js');
-const city = document.querySelector('.cities');
+const cities = document.querySelector('.cities');
+let somevalue = 'Wroclaw';
+var url;
+console.log(somevalue);
 
-let url = "http://api.weatherapi.com/v1/current.json?key=5ff4f415d6e94783821203729200701&q=" + city;
-// var valuse = "";
 
-// city.addEventListener('click', valuse = document.querySelector('.cities__item').getAttribute());
-
-console.log(value)
-// fetch('http://api.weatherapi.com/v1/current.json?key=5ff4f415d6e94783821203729200701&q='${valuse})
-fetch(url)
-    .then(resp => resp.json())
-    .then(resp => {
+cities.addEventListener('click', function (e) {
+  somevalue =   e.target.textContent;
+  url = "http://api.weatherapi.com/v1/current.json?key=3f1ad206d1b94436825173623201101&q=" + somevalue;
+  console.log(somevalue);
+  console.log(url);
+  fetch(url)
+      .then(resp => resp.json())
+      .then(resp => {
         const info = resp
         temp.innerHTML = info.current.temp_c + "°C";
         humidity.innerHTML = info.current.humidity + "%";
         clouds.innerHTML = info.current.cloud + "%";
         wind.innerHTML = info.current.wind_kph + " km/h";
-    })
-    .catch(err => {
+      })
+      .catch(err => {
         console.error("Błąd ładowania danych")
-    });
+      });
+});
+
+
 
 
 
