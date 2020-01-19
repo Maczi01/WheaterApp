d262c03325f51a30fdea51a2ffe7959154e8c715
Maczi01 <mateusz.w.twardy@gmail.com>
2020-01-19 15:13:50 +0100

    feat: add working today and tomorrow button and set background depending of conditions(first version)

--- src/js/main.js
+++ src/js/main.js
@@ -26,26 +26,27 @@ const searchButton = document.querySelector(".searchButton");
 const currentlocation = document.querySelector('.current__location');
 const cities = document.querySelector('.cities');
 const hamburger = document.querySelector('.hamburger--js');
+const today = document.querySelector(".current__days--today");
+const tomorrow = document.querySelector(".current__days--tomorrow");
+const time = document.querySelector('.current__time');
+const url = "http://api.weatherapi.com/v1/forecast.json?key=3f1ad206d1b94436825173623201101&q=";
+const main = document.querySelector(".main");
+let condition;
 let city;
-let time = document.querySelector('.current__time');
-var urlWheather;
-var urlAstro;
+let urlWheather;
 
 window.onload = function () {
-    time.innerHTML = new Date().getHours() - 1 + ":" + new Date().getMinutes();
+    time.innerHTML = new Date().getHours() + 3 + ":" + new Date().getMinutes() + 1;
     var loc = navigator.geolocation;
     if (loc) {
         loc.getCurrentPosition(function (location) {
             const lat = location.coords.latitude;
             const lon = location.coords.longitude;
-            fetch("http://api.weatherapi.com/v1/current.json?key=3f1ad206d1b94436825173623201101&q=" + lat + "," + lon)
-            // fetch("http://api.weatherapi.com/v1/forecast.json?key=3f1ad206d1b94436825173623201101&q=" + lat + "," + lon)
+            fetch(url + lat + "," + lon)
                 .then(resp => resp.json())
                 .then(resp => {
-                    const info = resp;
-                    city = info.location.name;
-                    console.log(city);
-                    getValues();
+                    city = resp.location.name;
+                    getValuesToday();
                 })
                 .catch(err => {
                     console.error("Błąd ładowania danych")
@@ -56,57 +57,98 @@ window.onload = function () {
     }
 };
 
-function getListener() {
-    cities.classList.toggle("cities--visible");
-}
+today.addEventListener('click', function () {
+    getValuesToday();
+});
 
-hamburger.addEventListener('click', getListener);
+tomorrow.addEventListener('click', function () {
+    getValuesTomorrow();
+});
+
+hamburger.addEventListener('click', showCityList);
 
 searchButton.addEventListener('click', function () {
     city = document.getElementById("customcity").value;
-    getValues();
-    getListener();
+    getValuesToday();
+    showCityList();
 });
 
 cities.addEventListener('click', function (e) {
     if (e.target.className === ("cities__item")) {
         city = e.target.textContent;
         console.log(city);
-        getValues();
-        getListener();
+        getValuesToday();
+        showCityList();
     }
 
 });
 
+function showCityList() {
+    cities.classList.toggle("cities--visible");
+}
 
-function getValues() {
+function getValuesToday() {
     currentlocation.innerHTML = city;
-    urlWheather = "http://api.weatherapi.com/v1/current.json?key=3f1ad206d1b94436825173623201101&q=" + city;
-    urlAstro = "http://api.weatherapi.com/v1/astronomy.json?key=3f1ad206d1b94436825173623201101&q=" + city + "&dt=2020-01-11";
+    urlWheather = url + city;
     fetch(urlWheather)
         .then(resp => resp.json())
         .then(resp => {
-            const info = resp
+            const info = resp;
             temp.innerHTML = info.current.temp_c + "°C";
             humidity.innerHTML = info.current.humidity + "%";
             clouds.innerHTML = info.current.cloud + "%";
             wind.innerHTML = info.current.wind_kph + " km/h";
+            condition = info.current.condition.text;
+            console.log(condition);
+            sunrise.innerHTML = info.forecast.forecastday[0].astro.sunrise;
+            sunset.innerHTML = info.forecast.forecastday[0].astro.sunset;
         })
         .catch(err => {
             console.error("Błąd ładowania danych")
         });
-    fetch(urlAstro)
+    switch (condition) {
+        case "Partly cloudy":
+            setBackgroundCloudy();
+            break;
+        case "Heavy snow":
+            setBackgroundSnowly();
+            break;
+        case "Sunny":
+            setBackgroundSunny();
+            break;
+        default:
+            main.style.backgroundColor = '$blue';
+    }
+}
+
+function getValuesTomorrow() {
+    currentlocation.innerHTML = city;
+    urlWheather = url + city + "&days=2";
+    fetch(urlWheather)
         .then(resp => resp.json())
         .then(resp => {
-            const info = resp
-            sunrise.innerHTML = info.astronomy.astro.sunrise;
-            sunset.innerHTML = info.astronomy.astro.sunset;
+            const info = resp;
+            temp.innerHTML = info.forecast.forecastday[1].day.maxtemp_c + "°C";
+            humidity.innerHTML = info.forecast.forecastday[1].day.avghumidity + "%";
+            clouds.innerHTML = "";
+            wind.innerHTML = info.forecast.forecastday[1].day.maxwind_kph + " km/h";
+            sunrise.innerHTML = info.forecast.forecastday[1].astro.sunrise;
+            sunset.innerHTML = info.forecast.forecastday[1].astro.sunset;
         })
         .catch(err => {
             console.error("Błąd ładowania danych")
         });
 }
 
+function setBackgroundSnowly() {
+    main.style.backgroundImage = 'url("../assets/img/snow.jfif")';
+}
+function setBackgroundCloudy() {
+    main.style.backgroundImage = 'url("../assets/img/rain.jpg")';
+}
+function setBackgroundSunny() {
+    main.style.backgroundImage = 'url("../assets/img/sunny.jpg")';
+}
 
 
 
